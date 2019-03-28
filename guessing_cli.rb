27d10a6bb66ab7de4ad619cccce013406c1020cf
Michelle M <michellemoon236@gmail.com>
2019-03-28 03:21:52 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,10 @@
 # Code your solution here!
+def run_guessing_game
+puts "Please guess a number:"
+answer = gets.chomp
+
+until answer.downcase == "exit"
+ if answer.to_s == rand
+end
+puts "Thank you for using this program"
+end
