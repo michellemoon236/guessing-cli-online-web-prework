e68de999a56ccf21f0a3b3ab1f2c620480b79189
Michelle M <michellemoon236@gmail.com>
2019-03-28 03:24:31 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,6 +5,9 @@ answer = gets.chomp
 
 until answer.downcase == "exit"
  if answer.to_s == rand
+   puts "You guesed the correct number!"
+ else
+   puts "The computer guessed #{rand}."
 end
 puts "Thank you for using this program"
 end
